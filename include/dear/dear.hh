11982deaf424a74d1035df1cccd7d7d8107a499d
Christian Menard <christian.menard@tu-dresden.de>
2019-09-26 11:10:12 +0200

    add a central header

---
+++ include/dear/dear.hh
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2019 TU Dresden
+ * All rights reserved.
+ *
+ * Authors:
+ *   Christian Menard
+ */
+
+#pragma once
+
+// include everything that is needed to use dear
+#include "action.hh"
+#include "environment.hh"
+#include "port.hh"
+#include "reactor.hh"
+#include "reactor.hh"
+#include "tag.hh"
+#include "time.hh"
