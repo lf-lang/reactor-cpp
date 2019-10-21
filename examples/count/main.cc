1e12e1590032d5d7092e63236da02f1f46995651
Christian Menard <christian.menard@tu-dresden.de>
2019-10-21 16:19:42 +0200

    add a simple counter example

---
+++ examples/count/main.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "dear/dear.hh"
+
+using namespace dear;
+
+class Count : public Reactor {
+ private:
+  // actions
+  Timer timer{"timer", this};
+  Action<int> counter{"counter", this};
+
+  // reactions
+  Reaction r_init{"r_init", 1, this, [this]() { init(); }};
+  Reaction r_counter{"r_counter", 2, this, [this]() { print_count(); }};
+
+ public:
+  Count(Environment* env) : Reactor("Count", env) {}
+
+  void assemble() override {
+    r_init.declare_trigger(&timer);
+    r_counter.declare_trigger(&counter);
+  }
+
+  void print_count() {
+    auto& value = *(counter.get());
+    std::cout << "Count: " << value << std::endl;
+    counter.schedule(value + 1, 1_s);
+  }
+
+  void init() {
+    std::cout << "Hello World!" << std::endl;
+    counter.schedule(0, 1_s);
+  }
+};
+
+int main() {
+  Environment e{4};
+
+  Count count{&e};
+  e.assemble();
+  e.init();
+
+  auto t = e.start();
+  t.join();
+
+  return 0;
+}
