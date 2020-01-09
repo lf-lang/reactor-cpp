e6e267749274c5d5360390c6f6c438771d8d816d
Christian Menard <christian.menard@tu-dresden.de>
2020-01-09 14:18:31 +0100

    Ensure proper cast of timepoints if system clock does not have nanosecdond resolution

--- lib/time.cc
+++ lib/time.cc
@@ -15,7 +15,8 @@ namespace reactor {
 
 std::ostream& operator<<(std::ostream& os, TimePoint tp) {
   char buf[20];
-  time_t time = std::chrono::system_clock::to_time_t(tp);
+  time_t time = std::chrono::system_clock::to_time_t(
+      std::chrono::time_point_cast<std::chrono::system_clock::duration>(tp));
   std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&time));
 
   auto epoch = std::chrono::duration_cast<Duration>(tp.time_since_epoch());
