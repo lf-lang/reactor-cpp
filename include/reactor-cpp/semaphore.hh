bea59bf354914b627385f3face5d118b9010883f
Christian Menard <christian.menard@tu-dresden.de>
2021-09-07 14:50:18 +0200

    wait for 1000us

--- include/reactor-cpp/semaphore.hh
+++ include/reactor-cpp/semaphore.hh
@@ -33,7 +33,7 @@ class Semaphore {
     while (count <= 0) {
       lg.unlock();
       using namespace std::chrono_literals;
-      std::this_thread::sleep_for(2000us);
+      std::this_thread::sleep_for(1000us);
       lg.lock();
     }
     count--;
