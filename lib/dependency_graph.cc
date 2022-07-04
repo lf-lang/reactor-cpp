d8e3a8bd24e1cd18b4a1edad09226c5c09829760
Christian Menard <christian.menard@tu-dresden.de>
2022-07-04 12:38:37 +0200

    annotate and color the graph

--- lib/dependency_graph.cc
+++ lib/dependency_graph.cc
@@ -1,17 +1,28 @@
-#include <fstream>
-
 #include "reactor-cpp/dependency_graph.hh"
 #include "reactor-cpp/port.hh"
 #include "reactor-cpp/reaction.hh"
 #include "reactor-cpp/reactor.hh"
+
 #include <boost/graph/directed_graph.hpp>
 #include <boost/graph/graphviz.hpp>
+#include <boost/graph/property_maps/constant_property_map.hpp>
+#include <boost/pending/property.hpp>
+#include <boost/property_map/function_property_map.hpp>
+
+#include <map>
+#include <set>
+#include <string>
 
 using namespace boost;
 
 namespace reactor {
 
-using DependencyGraph = directed_graph<>;
+struct reaction_info_t {
+  using kind = vertex_property_tag;
+};
+
+using ReactionProperty = property<reaction_info_t, const Reaction*>;
+using DependencyGraph = directed_graph<ReactionProperty>;
 using ReactionToVertexMap = std::map<const Reaction*, DependencyGraph::vertex_descriptor>;
 
 void populate_graph_with_reactions(DependencyGraph& graph, ReactionToVertexMap& vertex_map, const Reactor* reactor) {
@@ -72,8 +83,36 @@ void generate_dependency_graph(std::set<Reactor*>& top_level_reactors) {
     populate_graph_with_dependency_edges(graph, vertex_map, reactor);
   }
 
+  property_map<DependencyGraph, reaction_info_t>::type reaction_proprty_map = get(reaction_info_t(), graph);
+  for (auto entry : vertex_map) {
+    put(reaction_proprty_map, entry.second, entry.first);
+  }
+
+  dynamic_properties dp;
+  dp.property("node_id", get(boost::vertex_index, graph));
+  dp.property("label", make_function_property_map<DependencyGraph::vertex_descriptor, std::string>(
+                           [&reaction_proprty_map](DependencyGraph::vertex_descriptor vertex) {
+                             return boost::get(reaction_proprty_map, vertex)->name();
+                           }));
+  dp.property("tooltip", make_function_property_map<DependencyGraph::vertex_descriptor, std::string>(
+                             [&reaction_proprty_map](DependencyGraph::vertex_descriptor vertex) {
+                               return "container: " + boost::get(reaction_proprty_map, vertex)->container()->fqn();
+                             }));
+  dp.property("fillcolor",
+              make_function_property_map<DependencyGraph::vertex_descriptor, std::string>(
+                  [&reaction_proprty_map](DependencyGraph::vertex_descriptor vertex) {
+                    auto hash = std::hash<std::string>{}(boost::get(reaction_proprty_map, vertex)->container()->fqn());
+                    auto red = (hash & 0xff0000) >> 16; // NOLINT
+                    auto green = (hash & 0x00ff00) >> 9; // NOLINT
+                    auto blue = (hash & 0x0000ff); // NOLINT
+                    std::stringstream ss;
+                    ss << "#" << std::setfill('0') << std::setw(2) << std::hex << red << green << blue;
+                    return ss.str();
+                  }));
+  dp.property("style", make_constant_property<DependencyGraph::vertex_descriptor, std::string>("filled"));
+
   std::ofstream dot_file("test.dot");
-  write_graphviz(dot_file, graph);
+  write_graphviz_dp(dot_file, graph, dp);
 }
 
 } // namespace reactor
