089e290fb2d9e2cf645f39a0b603561037ea5bfe
Christian Menard <christian.menard@tu-dresden.de>
2022-08-04 19:42:51 +0200

    try to avoid the shared lock in SafeVector

--- include/reactor-cpp/safe_vector.hh
+++ include/reactor-cpp/safe_vector.hh
@@ -13,7 +13,6 @@
 #include <cstddef>
 #include <deque>
 #include <mutex>
-#include <shared_mutex>
 
 namespace reactor {
 
@@ -21,33 +20,29 @@ template <class T> class SafeVector {
 private:
   static constexpr std::size_t size_increment_{100};
   std::atomic<std::size_t> write_pos_{0};
-  std::size_t deque_size_{size_increment_};
+  std::atomic<std::size_t> deque_size_{size_increment_};
   std::deque<T> deque_{size_increment_};
-  std::shared_mutex mutex_;
+  std::mutex mutex_;
 
 public:
   void push_back(const T& value) {
-    auto pos = write_pos_.fetch_add(1, std::memory_order_release);
-    {
-      std::shared_lock<std::shared_mutex> lock(mutex_);
-      if (pos < deque_size_) {
-        deque_[pos] = value;
-      } else {
-        lock.unlock();
-        {
-          std::unique_lock<std::shared_mutex> unique_lock(mutex_);
-          while (pos < deque_size_) {
-            deque_size_ += size_increment_;
-            deque_.resize(deque_size_);
-          }
-          deque_[pos] = value;
-        }
+    auto pos = write_pos_.fetch_add(1, std::memory_order_relaxed);
+    auto size = deque_size_.load(std::memory_order_acquire);
+
+    if (pos < size) {
+      deque_[pos] = value;
+    } else {
+      std::lock_guard<std::mutex> lock(mutex_);
+      while (pos < deque_size_) {
+        deque_.resize(deque_size_);
+        deque_size_.fetch_add(size_increment_, std::memory_order_release);
       }
+      deque_[pos] = value;
     }
   }
 
   // careful! Using the iterators is only safe if no more elements are added to the vector!
-  auto begin() -> auto { return deque_.begin(); }
+  auto begin() -> auto{ return deque_.begin(); }
   auto end() -> auto{ return deque_.begin() + write_pos_.load(std::memory_order_acquire); }
 
   auto size() -> std::size_t { return write_pos_.load(std::memory_order_acquire); }
