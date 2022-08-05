53b975cae342ca0c523d3c55eb1cc68ad5238bbd
Christian Menard <christian.menard@tu-dresden.de>
2022-08-05 17:07:02 +0200

    fall back to shared mutex as races are otherwise difficult to avaoid

--- include/reactor-cpp/safe_vector.hh
+++ include/reactor-cpp/safe_vector.hh
@@ -12,6 +12,7 @@
 #include <atomic>
 #include <cstddef>
 #include <mutex>
+#include <shared_mutex>
 #include <vector>
 
 namespace reactor {
@@ -20,30 +21,35 @@ template <class T> class SafeVector {
 private:
   static constexpr std::size_t size_increment_{100};
   std::atomic<std::size_t> write_pos_{0};
-  std::atomic<std::size_t> vector_size_{size_increment_};
+  std::size_t vector_size_{size_increment_};
   std::vector<T> vector_{size_increment_};
-  std::mutex mutex_;
+  std::shared_mutex mutex_;
 
 public:
   void push_back(const T& value) {
-    auto pos = write_pos_.fetch_add(1, std::memory_order_acq_rel);
-    auto size = vector_size_.load(std::memory_order_acquire);
+    auto pos = write_pos_.fetch_add(1, std::memory_order_release);
 
-    if (pos < size) {
-      vector_[pos] = value;
-    } else {
-      std::lock_guard<std::mutex> lock(mutex_);
-      while (pos >= vector_size_) {
-        vector_size_.fetch_add(size_increment_, std::memory_order_release);
-        vector_.resize(vector_size_.load(std::memory_order_relaxed));
+    {
+      std::shared_lock<std::shared_mutex> shared_lock(mutex_);
+      if (pos < vector_size_) {
+        vector_[pos] = value;
+      } else {
+        shared_lock.unlock();
+        {
+          std::unique_lock<std::shared_mutex> unique_lock(mutex_);
+          while (pos >= vector_size_) {
+            vector_size_ += size_increment_;
+            vector_.resize(vector_size_);
+          }
+          vector_[pos] = value;
+        }
       }
-      vector_[pos] = value;
     }
   }
 
   // careful! Using the iterators is only safe if no more elements are added to the vector!
-  auto begin() -> auto { return vector_.begin(); }
-  auto end() -> auto { return vector_.begin() + write_pos_.load(std::memory_order_acquire); }
+  auto begin() -> auto{ return vector_.begin(); }
+  auto end() -> auto{ return vector_.begin() + write_pos_.load(std::memory_order_acquire); }
 
   auto size() -> std::size_t { return write_pos_.load(std::memory_order_acquire); }
   auto empty() -> bool { return size() == 0; }
